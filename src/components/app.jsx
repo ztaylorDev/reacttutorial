f414898fec1ee9cdd01f3a172abd13e9a1b90177
Zachary Taylor <zpanther8920@yahoo.com>
2021-07-21 18:27:00 -0400

    added the component folder and the app.jsx file and created a basic component

---
+++ src/components/app.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    state = { }
+    render(){
+        return(
+            <h1>Our React App Using Component</h1>
+        );
+
+
+    }
+
+
+}
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+import 'bootstrap/dist/css/bootstrap.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+const jsxElement = <h1>Our React App</h1>;
+console.log(jsxElement);
